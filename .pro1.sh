7bd49789286b241abd057e9fbd85830b5b6a56cf
vjp-DavidCR <72406491+vjp-DavidCR@users.noreply.github.com>
2022-11-23 20:59:58 +0100

    Add files via upload

--- .pro1.sh
+++ .pro1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-./.cron.sh
 
 #dialog --ok-label "Siguiente" --msgbox "¡Bienvenido!" 0 0
 
@@ -8,13 +7,29 @@
 #while [ $break == 0 ]
 #do
 
-dialog --menu "Opciones:" 0 0 0 1 "Programar nuevo escaneo" 2 "Listar escaneos programados" 3 "Borrar escaneos programados" 2>temp
+dialog --backtitle "Proyecto Shell Script" --title "Programar un escaneo" --calendar "Determine la fecha:" 0 0 2>temp
 Cancelado=$?
 Eleccion=`cat temp` ; rm temp
 if [ $Cancelado -eq 0 ]
   then echo "Has elegido: $Eleccion"
-  else dialog --infobox "¡Has cancelado!" 4 24
+  else dialog --backtitle "Proyecto Shell Script" --infobox "¡Has cancelado!" 4 24
 fi
+fecha=$Eleccion
+
+#sleep 1
+
+dialog --backtitle "Proyecto Shell Script" --title "Programar un escaneo" --timebox "Determine la hora:" 0 0 2>temp
+Cancelado=$?
+Eleccion=`cat temp` ; rm temp
+if [ $Cancelado -eq 0 ]
+  then echo "Has elegido: $Eleccion"
+  else dialog --backtitle "Proyecto Shell Script" --infobox "¡Has cancelado!" 4 24
+fi
+hora=$Eleccion
+
+
+
+./.cron.sh
 #sleep 1
 #done
 #clear
