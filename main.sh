54e0d86fc83aa9432eeef3fc6264ad61e3147a7d
vjp-DavidCR <72406491+vjp-DavidCR@users.noreply.github.com>
2022-11-23 19:04:04 +0100

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+dialog --ok-label "Siguiente" --msgbox "¡Bienvenido!" 0 0
+
+break=0
+while [ $break == 0 ]
+do
+
+dialog --menu "Opciones:" 0 0 0 1 "Escaneo de dispositivos" 2 "Programación de escaneos" 3 "Consultar escaneos de red" 4 "Consultar escaneos de servicios" 5 "Consultar escaneos de vulneravilidades" 0 "Salir" 2>temp
+Cancelado=$?
+Eleccion=`cat temp` ; rm temp
+if [ $Cancelado -eq 0 ]
+  then
+#echo "Has elegido: $Eleccion"
+    if [ $Eleccion == 1 ]
+    then
+      echo "Escaneo de dispositivos."
+      ./.red.sh
+      powermanga
+    elif [ $Eleccion == 2 ]
+    then
+      echo "Programación de escaneos."
+      ./.pro.sh
+    elif [ $Eleccion == 3 ]
+    then
+      echo "Consultar escaneos de red."
+      ./.cred.sh
+    elif [ $Eleccion == 4 ]
+    then
+      echo "Consultar escaneos de servicios."
+      ./.cser.sh
+    elif [ $Eleccion == 5 ]
+    then
+      echo "Consultar escaneos de vulnerabilidades."
+      ./.cvul.sh
+    elif [ $Eleccion == 0 ]
+    then
+      dialog --infobox "¡Hasta la próxima!" 4 24
+      break=1
+    else
+      echo "Si ves esto es que algo ha salido mal."
+      sleep 1
+    fi
+  else
+  dialog --infobox "¡Has cancelado!" 4 24
+fi
+sleep 1
+done
+clear
