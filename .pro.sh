e5447338380e345c609f92dada1cb180cc46e349
vjp-DavidCR <72406491+vjp-DavidCR@users.noreply.github.com>
2022-11-23 20:22:18 +0100

    Add files via upload

--- .pro.sh
+++ .pro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./.cron.sh
+#./.cron.sh
 
 #dialog --ok-label "Siguiente" --msgbox "¡Bienvenido!" 0 0
 
@@ -8,13 +8,34 @@
 #while [ $break == 0 ]
 #do
 
-dialog --menu "Opciones:" 0 0 0 1 "Programar nuevo escaneo" 2 "Listar escaneos programados" 3 "Borrar escaneos programados" 2>temp
+dialog --backtitle "Proyecto Shell Script" --menu "Opciones:" 0 0 0 1 "Programar nuevo escaneo" 2 "Listar escaneos programados" 3 "Borrar escaneos programados" 2>temp
 Cancelado=$?
 Eleccion=`cat temp` ; rm temp
 if [ $Cancelado -eq 0 ]
-  then echo "Has elegido: $Eleccion"
+  then
+#echo "Has elegido: $Eleccion"
+    if [ $Eleccion == 1 ]
+    then
+      echo "Programar nuevo escaneo."
+      ./.pro1.sh
+      ./.cron.sh
+    elif [ $Eleccion == 2 ]
+    then
+      echo "Listar escaneos programados."
+      ./.pro2.sh
+      ./.cron.sh
+    elif [ $Eleccion == 3 ]
+    then
+      echo "Borrar escaneos programados."
+      ./.pro3.sh
+      ./.cron.sh
+    else
+      echo "Si ves esto es que algo ha salido mal."
+      sleep 1
+    fi
   else dialog --infobox "¡Has cancelado!" 4 24
 fi
+
 #sleep 1
 #done
 #clear
---
+++ .pro1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+./.cron.sh
+
+#dialog --ok-label "Siguiente" --msgbox "¡Bienvenido!" 0 0
+
+#break=0
+#while [ $break == 0 ]
+#do
+
+dialog --menu "Opciones:" 0 0 0 1 "Programar nuevo escaneo" 2 "Listar escaneos programados" 3 "Borrar escaneos programados" 2>temp
+Cancelado=$?
+Eleccion=`cat temp` ; rm temp
+if [ $Cancelado -eq 0 ]
+  then echo "Has elegido: $Eleccion"
+  else dialog --infobox "¡Has cancelado!" 4 24
+fi
+#sleep 1
+#done
+#clear
