52174e69bfe09cf8098891faec533b1de441ad87
corchi <dcorchador01@informatica.iesvalledeljerteplasencia.es>
2022-11-28 20:42:57 +0100

    .git1

--- .pro1.sh
+++ .pro1.sh
@@ -11,7 +11,7 @@ dialog --backtitle "Proyecto Shell Script" --title "Programar un escaneo" --cale
 Cancelado=$?
 Eleccion=`cat temp` ; rm temp
 if [ $Cancelado -eq 0 ]
-  then echo "Has elegido: $Eleccion"
+  then echo "Has elegido: $Eleccion" > /dev/null 2>&1
   else dialog --backtitle "Proyecto Shell Script" --infobox "¡Has cancelado!" 4 24
 fi
 fecha=$Eleccion
@@ -22,14 +22,23 @@ dialog --backtitle "Proyecto Shell Script" --title "Programar un escaneo" --time
 Cancelado=$?
 Eleccion=`cat temp` ; rm temp
 if [ $Cancelado -eq 0 ]
-  then echo "Has elegido: $Eleccion"
+  then echo "Has elegido: $Eleccion" > /dev/null 2>&1
   else dialog --backtitle "Proyecto Shell Script" --infobox "¡Has cancelado!" 4 24
 fi
 hora=$Eleccion
 
+if [[ $(echo $fecha | tr "/" "-") < $(date '+%d-%m-%Y') ]] || [[ $(echo $hora) < $(date +"%T") ]] ;
+then
+  echo "break"
+  dialog --title "¡ERROR!" --backtitle "Proyecto Shell Script" --msgbox "El script no puede tener una fecha inferior a la actual." 0 0
+fi
 
+#date '+%d-%m-%Y'
+#echo $fecha | tr "/" "-"
+#date +"%T"
+#echo $hora
 
 ./.cron.sh
 #sleep 1
 #done
-#clear
+clear
