edb8503e71e469f6372efd0d4cca5ca2eda8517a
vjp-ismaellc <ilopezd01@informatica.iesvalledeljerteplasencia.es>
2022-12-01 13:59:05 -0500

    Seleccionar dispositivo

--- .red.sh
+++ .red.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 
-dialog --title "Selecconar Subred" --menu "Subredes" 0 0 0 "192.168.1.0/24" 1 "192.168.2.0/24" 2
-echo "$?"
+redes=$(
+contador=1
+for n in $(ip r | grep "/" | cut -f 1 -d " ")
+do
 
-sleep 2
+echo -n " $n"
+echo -n " $contador"
 
+contador=$(($contador + 1))
+
+done
+)
+
+subred=$(dialog --stdout --title "Selecconar Subred" --menu "Subredes" 0 0 0 $redes)
+
+
+if
+
+#Si hay redes haz esto
+
+ip=$(
+
+contador=1
+
+for n in $(nmap "$subred" | grep "scan report" | grep "(" | cut -f 2 -d "(" | cut -f 1 -d ")")
+
+do
+
+echo -n " $n"
+echo -n " $contador"
+
+contador=$(($contador + 1))
+
+done
+)
+
+subred=$(dialog --stdout --title "Selecconar Dispositivo" --menu "Dispositivos" 0 0 0 $ip)
+
+else
+
+echo "No se encontraron dispositivos"
