6b981047c2d8712b3072abdf149871aa116cab83
vjp-ismaellc <ilopezd01@informatica.iesvalledeljerteplasencia.es>
2022-12-02 10:50:44 -0500

    red

--- .red.sh
+++ .red.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 redes=$(
 contador=1
 for n in $(ip r | grep "/" | cut -f 1 -d " ")
@@ -15,10 +14,11 @@ done
 
 subred=$(dialog --stdout --title "Selecconar Subred" --menu "Subredes" 0 0 0 $redes)
 
+nmap "$subred" | grep "scan report" | grep "(" | cut -f 2 -d "(" | cut -f 1 -d ")" > ip.txt
 
-if
+cat ip.txt > $(echo 'RED/RED-'$(date '+%d-%m-%Y-%T'));
 
-#Si hay redes haz esto
+if [ -s ip.txt ] ; then
 
 ip=$(
 
@@ -38,6 +38,26 @@ done
 
 subred=$(dialog --stdout --title "Selecconar Dispositivo" --menu "Dispositivos" 0 0 0 $ip)
 
+respuesta=$(dialog --stdout --title "Seleccionar Escaneo" --menu "Tipos de escaneo" 0 0 0 1 "Escaneo de Servicios" 2 "Escaneo de Vulnerabilidades" 9 "Salir");
+
+	if [ "$respuesta" == 1  ] ; then
+		./.ser.sh $subred;
+	elif [ "$respuesta" == 2  ] ; then
+		./.vul.sh $subred;
+	elif [ "$respuesta" == 9  ] ; then
+		exit;
+	else
+		clear
+		echo "Has cancelado el script de gestión de usuarios"
+		sleep 2
+		clear
+		shell 2>/dev/null
+		exit
+	fi
+
 else
 
-echo "No se encontraron dispositivos"
+dialog --title "Seleccionar Dispositivo" --msgbox "No se encontraron dispositivos en la SUBRED" 0 0
+
+fi
+rm ip.txt
