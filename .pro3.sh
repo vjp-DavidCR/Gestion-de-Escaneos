41356f22e96dc92399d45ee62e2c87d3f13a50ae
vjp-DavidCR <dcorchador01@informatica.iesvalledeljerteplasencia.es>
2022-12-10 04:50:09 -0500

    pro3

--- .pro3.sh
+++ .pro3.sh
@@ -8,22 +8,25 @@
 #while [ $break == 0 ]
 #do
 
-lista=$(cat .cron.txt | tr " " "$" | grep -v "#")
+lista=$(cat .cron.txt | tr " " "$" | tr "*" "^" | grep -v "#")
 #echo $lista; exit
 
-options=$(
+touch temporal; rm temporal
+#options=$(
 contador=1
 for n in $(echo $lista)
 do
-echo -n " "$contador" \""
-echo -n $n | tr "$" " "
-echo -n "\" off"
+echo -n " "$contador "\"" >> temporal
+echo -n $n | tr "$" " " | tr "^" "*" >> temporal
+echo -n "\" off" >> temporal
 contador=$(($contador + 1))
 done
-)
-#echo $options; sleep 20
+#)
+#cat temporal; sleep 20
+rm temporal
 
-dialog --backtitle "Proyecto Shell Script" --buildlist "Opciones:"$options 0 0 0 2>temp
+dialog --backtitle "Proyecto Shell Script" --buildlist "Opciones:" f1 "0 5 * * 1 cat /var/backups/home.tgz 2>/dev/void" off f2 "0 5 * * 1 cat /var/backups/home.tgz 2>/dev/void" off f3 "0 5 * * 1 cat /var/backups/home.tgz 2>/dev/void" off f4 "0 5 * * 1 cat /var/backups/home.tgz 2>/dev/void" off f5 "0 5 * * 1 cat /var/backups/home.tgz 2>/dev/void" off f6 "0 5 * * 1 cat /var/backups/home.tgz 2>/dev/void" off 0 0 0 2>temp
+sleep 10
 Cancelado=$?
 Eleccion=`cat temp` ; rm temp
 if [ $Cancelado -eq 0 ]
