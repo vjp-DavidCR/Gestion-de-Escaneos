c0ce89168694ecec0e8b10d0f314fa2bcca27977
vjp-DavidCR <dcorchador01@informatica.iesvalledeljerteplasencia.es>
2022-12-12 12:05:30 +0000

    nmap mejorado

--- .vul.sh
+++ .vul.sh
@@ -2,13 +2,10 @@
 if ! [ -d "VUL" ]; then
 mkdir VUL
 fi
-dialog --backtitle "Proyecto Shell Script" --infobox "Inicializando..." 0 0
-#git clone https://github.com/scipag/vulscan scipag_vulscan 2>/dev/null
-#sudo ln -s `pwd`/scipag_vulscan /usr/share/nmap/scripts/vulscan
 clear
 #echo "Funciona"
 dialog --backtitle "Proyecto Shell Script" --infobox "Realizando escaneo de Vulnerabilidades. Esta operación puede tardar un poco..." 0 0
-nmap -sV --script=vulscan/vulscan.nse $1 > temp11
+nmap -p- --script=vuln $1 > temp11
 cat temp11 > $(echo 'VUL/VUL-'$(date '+%d-%m-%Y-%T'))
 if [[ $2 -ne 1 ]]
 then
