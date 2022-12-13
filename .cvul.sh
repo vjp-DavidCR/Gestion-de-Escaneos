3be223fc3b9f98a078e4c4c96f532feb8dd7dcdf
vjp-DavidCR <dcorchador01@informatica.iesvalledeljerteplasencia.es>
2022-12-13 11:09:30 +0000

    registros yesno

--- .cvul.sh
+++ .cvul.sh
@@ -18,6 +18,12 @@ Eleccion=`cat temp` ; rm temp
 if [ $Cancelado -eq 0 ]
   then
     dialog --backtitle "Proyecto Shell Script - David Corchado, Mario Carreras, Ismael Lopez y Jaime Gómez" --title "Listar Escaneos de Vulnerabilidades" --textbox "VUL/$Eleccion" 0 0
+    dialog --backtitle "Proyecto Shell Script - David Corchado, Mario Carreras, Ismael Lopez y Jaime Gómez" --title "Seleccionar Informe de Escaneo de VULNERABILIDADES" --yesno "¿Desea seleccionar otro INFORME?" 0 0 2>temp
+    Cancelado=$?
+    Eleccion=`cat temp` ; rm temp
+    if [ $Cancelado -ne 0 ]
+      then break=1
+    fi
   else
     dialog --backtitle "Proyecto Shell Script - David Corchado, Mario Carreras, Ismael Lopez y Jaime Gómez" --infobox "¡Has cancelado!" 4 24
     break=1
